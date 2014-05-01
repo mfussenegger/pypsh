10fe9c4e36b9dd441670c2750ef61880fa2ae9d0
Mathias Fussenegger <f.mathias@zignar.net>
2014-05-01 22:59:43 +0200

    update classifiers

--- setup.py
+++ setup.py
@@ -35,8 +35,12 @@ setup(
     classifiers=[
         'Development Status :: 4 - Beta',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation :: CPython',
         'Intended Audience :: System Administrators',
         'License :: OSI Approved :: MIT License',
         'Topic :: System :: Networking',
